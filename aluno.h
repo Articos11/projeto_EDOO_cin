83e576fa46e807b4a74b2426fc3e01803d4bbe27
Vinicius Caldas <79602224+Articos11@users.noreply.github.com>
2025-01-16 18:50:32 -0300

    Abacate

--- aluno.h
+++ aluno.h
@@ -1,21 +1,48 @@
 #include <string>
+#include <vector>
 #include "pessoa.h"
 using namespace std;
 
 #ifndef PROJETOEDO_ALUNO_H
 #define PROJETOEDO_ALUNO_H
 
-// Pra matriculas e pra notas, vai precisar de um setMatricula
+
 class Aluno : public Pessoa {
 private:
     int matricula;
-    double notas;
-
+    vector<double> notas;
 public:
-    Aluno(const string& novoNome = "", int novaIdade = 0 , int novaMatricula = 0000, double novasNotas = 0.0)
-    : Pessoa(novoNome, novaIdade), matricula(novaMatricula), notas(novasNotas) {}
+    Aluno(const string& novoNome = "", int novaIdade = 0 , int novaMatricula = 0000)
+    : Pessoa(novoNome, novaIdade), matricula(novaMatricula) {}
 
+    int setMatricula(int novaMatricula) {
+        if (novaMatricula > 0) {
+            matricula = novaMatricula;
+        } else {
+            cout << "Essa matrícula é inválida." << endl;
+        }
+    }
 
+    int getMatricula() const {return matricula;}
+
+    vector<double> getNotas() const {return notas;}
+
+    // Adicionamos um método para que o professor possa adicionar as notas do aluno.
+    void adicionarNota(double novaNota) {
+        notas.push_back(novaNota);
+    }
+    // Calculo da média do aluno.
+    double calculaMedia() {
+        if (notas.size() < 4) {
+            cout << "Ainda faltam atividades para serem realizadas pelo aluno." << endl;
+            return 0.0;
+        }
+        double soma = 0.0;
+        for (int i = 0; i < 4; i++) {
+            soma += notas[i];
+        }
+        return (soma / 4);
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "pessoa.h"
 #include "aluno.h"
 using namespace std;
 
 
 int main() {
-
+    double Nota;
     // O construtor só funciona de maneira "geral" para o código quando está fora de um {}.
     // Caso contrário, o destrutor será imediatamente chamado, destruindo aquela classe.
     Pessoa pessoa1("Joao", 30);
-    Aluno aluno1("Pedro", 30, 2504, 9.0);
+    Aluno aluno1("Pedro", 30, 2504);
 
-    cout << aluno1.getNome() << " tem " << aluno1.getIdade() << "anos, portador da matricula ";
+    cout << aluno1.getNome() << " tem " << aluno1.getIdade() << " anos, portador da matricula " << aluno1.getMatricula() << endl;
 
+    // Aqui adicionamos as notas.
+    for (int i = 0; i < 4; i++) {
+        cout << "Adicione a nota " << i + 1 << " do aluno:";
+        cin >> Nota;
+        aluno1.adicionarNota(Nota);
+    }
 
+    vector<double> notas = aluno1.getNotas();
+    for (double nota : notas) {
+        cout << nota << " ";
+    }
+
+    cout << aluno1.calculaMedia();
 
     return 0;
 }
